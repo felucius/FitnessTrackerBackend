8cad47018ed4254df8f2d62d8159704c2770185e
felucius <maxime.de.lange@outlook.com>
2025-09-16 13:57:14 +0200

    Added Workoutplan DTOs CreateWorkoutPlanRequest, WorkoutPlanResponse and WorkoutPlanDetailedResponse. Also added mapping

---
+++ FitnessTrackerBackend/FitnessTrackerBackend/Dto/WorkoutPlans/WorkoutPlanDetailedResponse.cs
@@ -0,0 +1,18 @@
+﻿using FitnessTrackerBackend.Dto.Exercises;
+using FitnessTrackerBackend.Models;
+
+namespace FitnessTrackerBackend.Dto.WorkoutPlans
+{
+    public sealed record WorkoutPlanDetailedResponse
+    (
+        Guid Id,
+        Guid UserId,
+        User? User,
+        string Name,
+        string Type,
+        string? Description,
+        int? Frequency,
+        IReadOnlyList<ExerciseListItemResponse> Exercises
+    // WIP: need more fields for workoutplan details
+    );
+}
---
+++ FitnessTrackerBackend/FitnessTrackerBackend/Dto/WorkoutPlans/WorkoutPlanResponse.cs
@@ -0,0 +1,15 @@
+﻿using FitnessTrackerBackend.Models;
+
+namespace FitnessTrackerBackend.Dto.WorkoutPlans
+{
+    public sealed record WorkoutPlanResponse
+    (
+        Guid Id,
+        Guid UserId,
+        User? User,
+        string Name,
+        string Type,
+        string? Description,
+        int? Frequency
+    );
+}
