ffc37610959770d6faae0c56a908977ccc90857b
felucius <maxime.de.lange@outlook.com>
2025-09-16 09:28:59 +0200

    Added calendar events. Added one Calendar event response and one request. Also added a mapping for future testing

---
+++ FitnessTrackerBackend/FitnessTrackerBackend/Dto/CalendarEvents/CalendarEventResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace FitnessTrackerBackend.Dto.CalendarEvents
+{
+    public sealed record CalendarEventResponse (
+        
+        Guid Id,
+        string Title,
+        Guid WorkoutPlanId,
+        DateTimeOffset Start,
+        DateTimeOffset End,
+        bool AllDay
+
+        // Additions will be coming in later
+    );
+}
---
+++ FitnessTrackerBackend/FitnessTrackerBackend/Dto/Mappings/CalendarEventMappings.cs
@@ -0,0 +1,28 @@
+﻿using FitnessTrackerBackend.Dto.CalendarEvents;
+using FitnessTrackerBackend.Models;
+
+namespace FitnessTrackerBackend.Dto.Mappings
+{
+    public static class CalendarEventMappings
+    {
+        public static CalendarEventResponse ToResponse(this CalendarEvent calendarEvent) =>
+            new CalendarEventResponse(
+                calendarEvent.Id,
+                calendarEvent.Title,
+                calendarEvent.WorkoutPlanId,
+                calendarEvent.Start,
+                calendarEvent.End,
+                calendarEvent.AllDay
+            );
+
+        public static CalendarEvent ToModel(this CreateCalendarEventRequest request) =>
+            new CalendarEvent
+            {
+                Title = request.Title,
+                WorkoutPlanId = request.WorkoutPlanId,
+                Start = request.Start,
+                End = request.End,
+                AllDay = request.AllDay
+            };
+    }
+}
