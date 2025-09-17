d508549254d7519ab47d1612a87f787e88a790b8
felucius <maxime.de.lange@outlook.com>
2025-09-17 09:40:36 +0200

    Added Progression DTOs CreateExerciseProgressionRequest and ExerciseProgressionResponse. Also created the mapping

---
+++ FitnessTrackerBackend/FitnessTrackerBackend/Dto/Progressions/ExerciseProgressionResponse.cs
@@ -0,0 +1,15 @@
+﻿using FitnessTrackerBackend.Models;
+
+namespace FitnessTrackerBackend.Dto.Progressions
+{
+    public sealed record ExerciseProgressionResponse
+    (
+        Guid Id,
+        Guid? UserId,
+        string ExerciseId,
+        Exercise Exercise,
+        DateTime Date,
+        int Weight,
+        int Reps
+    );
+}
